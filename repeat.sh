eb9b7b638b409461d33c86c71f5a4f3c49d79fb2
marinacler <36500322+marinacler@users.noreply.github.com>
2018-02-15 11:12:27 +0100

    Add files via upload
    Adding the first file

---
+++ repeat.sh
@@ -0,0 +1,9 @@
+cd Desktop/
+mkdir projects
+cd projects/
+mkdir MTLSproject
+touch MTLSproject/readme.txt  #touch for creating a file inside a directory
+touch MTLSproject/commands.txt
+mkdir datasets
+mkdir general_scripts
+mkdir bin
