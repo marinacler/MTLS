e3c72f4888070551ef8d661d94dee4f79b32ef4c
Marina <marina.martinez.hernandez@stud.ki.se>
2018-03-01 13:30:38 +0100

    Commiting this

---
+++ repeat.sh
@@ -0,0 +1,8 @@
+mkdir projects
+cd projects/
+mkdir MTLSproject
+touch MTLSproject/readme.txt  #touch for creating a file inside a directory
+touch MTLSproject/commands.txt
+mkdir datasets
+mkdir general_scripts
+mkdir bin
